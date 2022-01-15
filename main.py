9a7e1efe47f7a282a4b90af9e72e2f5bbe548f00
DavidC <adcc662@gmail.com>
2022-01-15 10:56:57 -0600

    cookies_and_headers

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from enum import Enum
 #Pydantic
 from pydantic import BaseModel,HttpUrl, ValidationError
 from pydantic import Field
+from pydantic import EmailStr
 #FastAPI
 from fastapi import status
 from fastapi import FastAPI
-from fastapi import Body, Query, Path, Form
+from fastapi import Body, Query, Path, Form, Header, Cookie
 
 app = FastAPI()
 
@@ -177,4 +178,29 @@ def update_person(
 def login(username:str = Form(...),password:str=Form(...)):
     return LoginOut(username=username)
 
-    
+#Cookies and Headers parameters
+
+@app.post(
+    path="/contact",
+    status_code = status.HTTP_200_OK,
+)
+def contact(
+    first_name : str = Form(
+        ...,
+        max_Length=20,
+        min_Length=1,
+        ),
+    last_name : str = Form(
+        ...,
+        max_Length=20,
+        min_Length=1,
+        ),
+    email: EmailStr = Form(...),
+    message: str = Form(
+        ...,
+        min_Length=20,
+    ),
+    user_agent: Optional[str] = Header(default=None),
+    ads: Optional[str] = Cookie(default=None)
+):
+    return user_agent
